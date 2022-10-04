3ded71042e473342e860c8128e2addcd8c07dee2
Alex Racape <alex.racape@gmail.com>
2022-10-04 11:01:31 -0400

    Got basic window to work - nothing renders yet - need to figure out callbacks/ threading

--- run_client.py
+++ run_client.py
@@ -1,9 +1,35 @@
 import penne
+import moderngl
+import moderngl_window as mglw
+
 from tagliatelle.delegates import *
+
+class Test(mglw.WindowConfig):
+    gl_version = (3, 3)
+    window_size = (1920, 1080)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # Do initialization here
+        print(type(self.ctx))
+        # self.prog = self.ctx.program(...) # create program in moderngl context for shaders
+        # self.vao = self.ctx.vertex_array(...)
+        # self.texture = self.ctx.texture(self.wnd.size, 4)
+
+    def render(self, time, frametime):
+        # This method is called every frame
+        # self.vao.render()
+        pass
+
+    def resize(self, width: int, height: int):
+        print("Window was resized. buffer size is {} x {}".format(width, height))
+
+    def close(self):
+        print("The window is closing")
 
 
 def start_render():
-    pass
+    mglw.run_window_config(Test)
 
 
 del_hash = {
@@ -24,7 +50,9 @@ del_hash = {
 }
 
 def start():
-    client = penne.create_client("ws://localhost:50000", del_hash, on_connected=start_render)
+    client = penne.create_client("ws://localhost:50000", del_hash)
+    start_render()
+    client.thread.join()
 
 if __name__ == "__main__":
     start()
