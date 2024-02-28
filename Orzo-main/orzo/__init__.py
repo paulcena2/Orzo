141933b6de13d334c48464a6ef68125d90a3687c
paulcena2 <135997381+paulcena2@users.noreply.github.com>
2024-02-28 17:39:07 -0600

    Delete Orzo-main directory

--- Orzo-main/orzo/__init__.py
+++
@@ -1,5 +0,0 @@
-"""A python client application geared towards rendering 3D scenes with NOODLES"""
-
-__version__ = "0.4.0"
-
-from .core import run, connect, Client
--- Orzo-main/tests/test_render.py
+++
@@ -1,8 +0,0 @@
-
-from .servers import base_server
-from orzo.delegates import *
-
-
-def test_get_world_transform():
-
-    pass
